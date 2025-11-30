3fbcc597779de0a4629bffeb5aa43e30acfd4dc7
itsomg134 <omgedam123098@gmail.com>
2025-11-30 19:45:18 +0530

    Add files via upload

---
+++ E-commerce Ai Voice shopping files/frontened/middleware-build-manifest.js
@@ -0,0 +1,28 @@
+globalThis.__BUILD_MANIFEST = {
+  "pages": {
+    "/_app": []
+  },
+  "devFiles": [],
+  "ampDevFiles": [],
+  "polyfillFiles": [
+    "static/chunks/d4dc9_next_dist_build_polyfills_polyfill-nomodule.js"
+  ],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_d118b270._.js",
+    "static/chunks/d4dc9_next_dist_compiled_react-dom_6201cce4._.js",
+    "static/chunks/d4dc9_next_dist_compiled_next-devtools_index_12f8f4e8.js",
+    "static/chunks/d4dc9_next_dist_compiled_01d0f172._.js",
+    "static/chunks/d4dc9_next_dist_client_0688443b._.js",
+    "static/chunks/d4dc9_next_dist_c0ab6dd2._.js",
+    "static/chunks/eb684_@swc_helpers_cjs_1bcdfe7a._.js",
+    "static/chunks/OneDrive_Desktop_E-commerce Agent_frontend_a0ff3932._.js",
+    "static/chunks/turbopack-OneDrive_Desktop_E-commerce Agent_frontend_cdd5b54b._.js"
+  ],
+  "ampFirstPages": []
+};
+globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
